5ece828bdf25ee4908b45c18e9d11f0345ff1a87
partha-0103 <partha@bureau.id>
2022-09-25 22:16:08 +0530

    fix 6

--- src/PlanSelectorButton.js
+++ src/PlanSelectorButton.js
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { useAction, useFindFirst } from "@gadgetinc/react";
-import { Layout, Card, Button } from "@shopify/polaris";
+import { Layout, Card, Button, Page, List } from "@shopify/polaris";
 import { api } from "./api";
 export const PlanSelectorButton = (props) => {
   const [result, refresh] = useFindFirst(api.shopifyShop, {
@@ -11,23 +11,32 @@ export const PlanSelectorButton = (props) => {
   const { data, error, fetching } = result;
 
   return (
-    <Layout>
-      <Layout.Section>
-        <Card sectioned>
-          <p>Product Pricing Information</p>
-          <div>Inr 10 per order</div>
-        </Card>
-        <Button
-          onClick={() => {
-            window.open(data?.confirmationurl);
-            window.close();
-          }}
-          disabled={fetching}
-          primary
-        >
-          Accept
-        </Button>
-      </Layout.Section>
-    </Layout>
+    <Page fullWidth>
+      <Layout>
+        <Layout.Section>
+          <Card sectioned title="Product Pricing Information">
+            <p>INR 10 per order</p>
+            <p>Services Included</p>
+            <List type="bullet">
+              <List.Item>Address COmpleteness Scoring</List.Item>
+              <List.Item>RTO risk flagging Red/Green</List.Item>
+              <List.Item>Rules Customization for your business</List.Item>
+              <List.Item>Pincode correctness Check</List.Item>
+              <List.Item>Cod order frequency</List.Item>
+            </List>
+            <Button
+              onClick={() => {
+                window.open(data?.confirmationurl);
+                window.close();
+              }}
+              disabled={fetching}
+              primary
+            >
+              Accept
+            </Button>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
   );
 };
