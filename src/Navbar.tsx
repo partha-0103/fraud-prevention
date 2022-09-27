6796d59638e85f90a80263e9333fdccde4fda20a
partha-0103 <partha@bureau.id>
2022-09-27 17:22:54 +0530

    nav bar 5

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,22 +1,40 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <div className="nav">
       <ul>
         <Link href="/dashboard">
-          <a className="nav-link">
+          <a
+            className={
+              router.pathname === "dashboard"
+                ? "nav-link active-nav"
+                : "nav-link"
+            }
+          >
             <li>Dasshboard</li>
           </a>
         </Link>
         <Link href="/">
-          <a className="nav-link">
+          <a
+            className={
+              router.pathname === "" ? "nav-link active-nav" : "nav-link"
+            }
+          >
             <li>Settings</li>
           </a>
         </Link>
-        <Link href="/blog">
-          <a className="nav-link">
+        <Link href="/payment-confirmation">
+          <a
+            className={
+              router.pathname === "payment-confirmation"
+                ? "nav-link active-nav"
+                : "nav-link"
+            }
+          >
             <li>Billing</li>
           </a>
         </Link>
